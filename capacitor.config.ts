5232c9602138532df0cbe54c1af15b82d6648d31
Michael Reiniger <m.reiniger@silverorbit.de>
2025-04-12 18:50:27 +0200

    Update Capacitor configuration: Changed appId to 'de.codewald.apps.clicker' while keeping appName and webDir unchanged for consistency in project settings.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.michaelreiniger.apps.clicker',
-  appName: 'Clicker',
-  webDir: 'dist/clickerapp/browser'
+    appId: 'de.codewald.apps.clicker',
+    appName: 'Clicker',
+    webDir: 'dist/clickerapp/browser',
 };
 
 export default config;
